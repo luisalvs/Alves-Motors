068bd593f8b10e427bfa95612c59d2fa1868b996
luisalvs <luisalvst@gmail.com>
2025-09-25 17:07:49 -0300

    Create db.py

---
+++ db.py
@@ -0,0 +1,45 @@
+import mariadb
+import sys
+
+
+try:
+
+    conn = mariadb.connect(
+        user='root',
+        password='31142611',
+        port=3306,
+        database='alves_motors'
+    )
+
+except mariadb.Error as e:
+    print(f'Erro ao conectar ao MariaDB: {e}')
+    sys.exit(1)
+
+
+def create_table():
+    cur = conn.cursor()
+    cur.execute("""
+        create table if not exists veiculos (
+            id integer primary key auto_increment,
+            modelo text,
+            marca text,
+            cor text,
+            ano integer
+        )
+        """)
+
+
+def add_veiculo(modelo, marca, cor, ano):
+    cur = conn.cursor()
+    cur.execute('insert into veiculos (modelo, marca, cor, ano) values (?, ?, ?, ?)',
+                (modelo, marca, cor, ano))
+    conn.commit()
+
+def excluir_veiculo():
+    cur = conn.cursor()
+    cur.execute()
+
+def listar_veiculos():
+    cur = conn.cursor()
+    cur.execute('select modelo, marca, cor, ano from veiculos')
+    return cur.fetchall()
